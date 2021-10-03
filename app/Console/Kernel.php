5df91a95ec045149e508eabb1be8cf2beff809df
Felipe Pedrosa de Almeida <lipe_fit@hotmail.com>
2021-10-02 23:02:05 -0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('report:cron')->dailyAt('22:40');
+        $schedule->command('report:cron')->dailyAt('18:00');
     }
 
     /**
